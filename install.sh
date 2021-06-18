282ffdb5f3911619359fe3dfa089e0563e08133c
Sebastien Dorgan <sebastien.dorgan@csgroup.eu>
2021-06-18 12:38:47 +0000

    start tests on ovh

--- install.sh
+++ install.sh
@@ -1,22 +1,25 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y build-essential apt-transport-https ca-certificates \
-    lsb-release gnupg make wget curl unzip vim git python3 python3-pip docker-compose
+    lsb-release gnupg make wget curl unzip vim git python3 python3-pip docker-compose conntrack
 
 # Install  docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
- sudo apt-get update
- sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Install docker-compose
-pip install docker-compose --user
+pip install docker-compose sarge python-keycloak requests --user
 
 # Configure user environment
 source metis.env
 
+cd assets/admin-plane
+docker-compose up -d registry
+cd -
 # Install and configure minikube
 ## minikube
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
@@ -33,19 +36,16 @@ sudo ./get_helm.sh
 rm -f get_helm.sh
 
 # add helm repositories
-helm add_repo bitnami https://charts.bitnami.com/bitnami
-helm update repo
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
 
 # start minikube with Project Calico CNI
-minikube addons configure registry-creds
-minikube addons enable registry-creds
-minikube start --network-plugin=cni --cni=calico --kubernetes-version v1.18.16 --driver=none
+minikube start --network-plugin=cni --cni=calico --kubernetes-version=v${KUBE_VERSION} --driver=none --insecure-registry=${PRIVATE_IP}
 
 # Run minikube tunnel to enable Kubernetes LoadBalancer
 nohup minikube tunnel > /dev/null&
 
 cd cli
-python install_admin_plane.py
-python install_data_plane.py
+python3 install_admin_plane.py
 cd -
 
