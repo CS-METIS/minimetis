296012b6f71eb858cccf81dd1a1e879b2aca3978
Sebastien Dorgan <sebastien.dorgan@csgroup.eu>
2021-06-18 19:04:53 +0200

    local test

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 # Install docker-compose
 pip install docker-compose sarge python-keycloak requests --user
 
-# Configure user environment
+Configure user environment
 source metis.env
 
 cd assets/admin-plane
@@ -29,6 +29,13 @@ sudo mv minikube /usr/local/bin
 curl -LO https://storage.googleapis.com/kubernetes-release/release/v${KUBE_VERSION}/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin
+
+## crictl
+VERSION="v1.21.0"
+wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
+sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
+rm -f crictl-$VERSION-linux-amd64.tar.gz
+
 ## helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod +x get_helm.sh
