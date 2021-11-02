87ec297dad8785f8c3952735ec856ce1ff7b1f62
baptiste <baptiste.aussel@sgroup.eu>
2021-11-02 15:30:54 +0000

    baptiste

--- cli/add_dataminer_plane.py
+++ cli/add_dataminer_plane.py
@@ -2,13 +2,21 @@ from metis_lib import miningservices
 from getpass import getpass
 
 if __name__ == "__main__":
-    user_email = input("Enter miner email address\n>>")
-    user_firstname = input("Enter user firstname\n>>")
-    user_lastname = input("Enter user lastname\n>>")
-    user_name = input("Enter miner user name\n>>")
-    user_password = getpass("Enter user password\n>>")
-    user_cpu_limit = input("Enter user cpu limit (ex: 4)\n>>")
-    user_memory_limit = input("Enter user memory limit (ex: 8Gi)\n>>")
+    # user_email = input("Enter miner email address\n>>")
+    # user_firstname = input("Enter user firstname\n>>")
+    # user_lastname = input("Enter user lastname\n>>")
+    # user_name = input("Enter miner user name\n>>")
+    # user_password = getpass("Enter user password\n>>")
+    # user_cpu_limit = input("Enter user cpu limit (ex: 4)\n>>")
+    # user_memory_limit = input("Enter user memory limit (ex: 8Gi)\n>>")
+
+    user_email = "baptiste.aussel@csgroup.eu"
+    user_firstname = "baptiste"
+    user_lastname = "Aussel"
+    user_name = "demo"
+    user_password = "toto"
+    user_cpu_limit = 4
+    user_memory_limit = "8Gi"
 
     miningservices.deploy(
         email=user_email,
